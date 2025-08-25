def0f4fa81f8c436de7da900771b3618bd4d8c38
akshar51 <aksharparekh401@gmail.com>
2025-08-25 16:37:35 +0530

    done

--- controller/blog.controller.js
+++ controller/blog.controller.js
@@ -1,3 +1,5 @@
+const User = require("../model/userAuth")
+
 module.exports.homePage = (req,res)=>{
     res.render('pages/index')
 }
@@ -8,4 +10,15 @@ module.exports.signupPage = (req,res)=>{
 
 module.exports.loginPage = (req,res)=>{
     res.render('pages/login')
+}
+
+module.exports.signup = async (req,res)=>{
+    try {
+        let user = await User.create(req.body)
+        console.log(user)
+        res.redirect('/login')
+    } catch (error) {
+        console.log(error)
+        res.redirect('/signup')
+    }
 }
--- router/index.js
+++ router/index.js
@@ -1,10 +1,11 @@
 const { Router } = require("express");
-const { homePage ,signupPage,loginPage} = require("../controller/blog.controller");
+const { homePage ,signupPage,loginPage, signup} = require("../controller/blog.controller");
 
 const router = Router()
 
 router.get('/',homePage)
 router.get('/signup',signupPage)
 router.get('/login',loginPage)
+router.post('/signup',signup)
 
 module.exports = router
