9af811d17c8b584c6ca769e6fabccdd1c5ef5c28
akshar51 <aksharparekh401@gmail.com>
2025-08-28 16:33:32 +0530

    done

--- configs/database.js
+++ configs/database.js
@@ -3,7 +3,7 @@ const { default: mongoose } = require("mongoose");
 const db = async (req, res) => {
   try {
     await mongoose.connect(
-      "mongodb+srv://shubhamparekh04:1234@cluster0.ycafm.mongodb.net/blog"
+      "mongodb+srv://aksharparekh401:12345@cluster0.ncwztql.mongodb.net/Blog"
     );
     console.log("Database is connected...!");
   } catch (error) {
